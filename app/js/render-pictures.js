9c370ad1c74c686eac0d6bd286f7b6e48f32fb70
Basket Son <ehal.bydlo@gmail.com>
2019-07-13 19:46:21 +0300

    Разделение отрисовки на функции

--- app/js/render-pictures.js
+++ app/js/render-pictures.js
@@ -18,29 +18,39 @@
 
   var generatedPictures = window.createMockPhotoArray();
 
-  window.renderPictures = function () {
+  window.renderPictures = function (picsArray) {
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < generatedPictures.length; i++) {
-      fragment.appendChild(renderPicture(generatedPictures[i]));
+    for (var i = 0; i < picsArray.length; i++) {
+      fragment.appendChild(renderPicture(picsArray[i]));
     }
     picturesContainer.appendChild(fragment);
   };
 
   var bigPicture = document.querySelector('.modal-picture');
 
-  bigPicture.classList.remove('hidden');
-  bigPicture.querySelector('.modal-picture__img img').src = generatedPictures[0].url;
-  bigPicture.querySelector('.likes-count').textContent = generatedPictures[0].likes;
-  bigPicture.querySelector('.comments-count').textContent = generatedPictures[0].comments.length;
-  for (var i = 0; i < generatedPictures[0].comments.length; i++) {
-    bigPicture.querySelector('.social__comments').insertAdjacentHTML('beforeEnd',
-        '<li class="social__comment social__comment--text"><img class="social__picture" src="img/avatar-' +
-Math.ceil(Math.random() * 6) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35"><p class="social__text">' +
-generatedPictures[0].comments[i] + '</p></li>');
-  }
-  bigPicture.querySelector('.social__caption').textContent = generatedPictures[0].description;
-  bigPicture.querySelectorAll('.social__comment-count, .social__comments-loader').forEach(function (it) {
-    it.classList.add('visually-hidden')
-  })
+  window.renderMockBigPic = function (picsArray) {
+    bigPicture.classList.remove('hidden');
+    bigPicture.querySelector('.modal-picture__img img').src = picsArray[0].url;
+    bigPicture.querySelector('.likes-count').textContent = picsArray[0].likes;
+    bigPicture.querySelector('.comments-count').textContent = picsArray[0].comments.length;
+    for (var i = 0; i < picsArray[0].comments.length; i++) {
+      bigPicture.querySelector('.social__comments').insertAdjacentHTML('beforeEnd',
+          '<li class="social__comment social__comment--text">' +
+      '<img class="social__picture" src="img/avatar-' +
+      Math.ceil(Math.random() * 6) +
+      '.svg" alt="Аватар комментатора фотографии" width="35" height="35">' +
+      '<p class="social__text">' +
+      picsArray[0].comments[i] +
+      '</p></li>');
+    }
+    bigPicture.querySelector('.social__caption').textContent = picsArray[0].description;
+    bigPicture.querySelectorAll('.social__comment-count, .social__comments-loader').forEach(function (it) {
+      it.classList.add('visually-hidden');
+    });
+  };
+
+  window.renderMockBigPic(generatedPictures);
+  window.renderPictures(generatedPictures);
+
 })();
